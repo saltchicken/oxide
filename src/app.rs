069a92508d3c39b98df2c213b105577b3b978603
saltchicken <john.eicher89@gmail.com>
2025-10-24 16:13:37 -0700

    Minor adjustments

--- src/app.rs
+++ src/app.rs
@@ -104,10 +104,8 @@ impl eframe::App for TemplateApp {
                     // ui.add(egui_demo_lib::egui_github_link_file!());
                 });
 
-            ui.separator();
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
                 egui::warn_if_debug_build(ui);
             });
         });
@@ -267,18 +265,4 @@ impl RotatingTriangle {
             gl.draw_arrays(glow::TRIANGLES, 0, 3);
         }
     }
-}
-
-fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
-    ui.horizontal(|ui| {
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.label("Powered by ");
-        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-        ui.label(" and ");
-        ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
-        );
-        ui.label(".");
-    });
 }
